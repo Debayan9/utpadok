a4f9c5b8f397c71d4b7be87b9dd8d8c46cb5c1dc
Debayan9 <dsweb2019@gmail.com>
2019-02-08 23:35:42 +0530

    little change

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 ﻿$(document).ready(function() {
     $("#InlineFrame1").hide();
     $("#wb_Text2").hide();
+    $("#wb_Text1").css({"background-color":"#FFFFFF"});
     $("#Button1").click(function() {
         var a = $("#Editbox1").val();
         var s1 = dsmath.factor(a);
         var s2 = s1.toString();
-
+        var s3=s2.replace(/,/g ,", ")
         $("#wb_Text2").show();
-        $("#wb_Text2").text("উৎপাদক গুলো= " + s2);
+        $("#wb_Text2").text("উৎপাদক গুলো= " + s3);
 
     });
     //alert("test loading script");
