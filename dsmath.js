b1897b89a6fe2db26e530a60c0e71522a6437e14
Debayan9 <dsweb2019@gmail.com>
2019-02-08 22:07:37 +0530

    test

---
+++ dsmath.js
@@ -0,0 +1,16 @@
+﻿var dsmath={
+    factor: function(num) { //create all factor of 'num' as array 'factor[]'
+                        this.num = num;
+                        n = this.num;
+                        var m = 0;
+                        factor = [];
+                        for (var i = 0; i <= n / 2; i = i + 1) {
+                                if (n % i == 0) {
+                                        factor[m] = i;
+                                        m = m + 1;
+                                }
+                        }
+                        factor[m]=n;
+            return factor;
+                }
+        }
---
+++ script.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function() {
+    $("#InlineFrame1").hide();
+    $("#wb_Text2").hide();
+    $("#Button1").click(function() {
+        var a = $("#Editbox1").val();
+        var s1 = dsmath.factor(a);
+        var s2 = s1.toString();
+
+        $("#wb_Text2").show();
+        $("#wb_Text2").text("উৎপাদক গুলো= " + s2);
+
+    });
+    //alert("test loading script");
+});
